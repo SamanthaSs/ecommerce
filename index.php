0b59e386c8085238e755d169a51b464edb4d9e58
SamanthaSs <samanthaschmoeller@gmail.com>
2021-11-08 17:09:13 -0300

    Criação e edição de categorias

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ use \Hcode\PageAdmin;
 use \Hcode\Model;
 use \Hcode\Model\User;
 use \Hcode\Mailer;
+use \Hcode\Model\Category;
 
 $app = new Slim();
 
@@ -236,6 +237,91 @@ $app->post("/admin/forgot/reset",function(){
     $page->setTpl("forgot-reset-sucess");
 
 });
+
+$app->get("/admin/categories",function(){
+
+    User::verifyLogin();
+    
+    $categories = Category::listAll();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("categories",[
+        'categories'=>$categories
+    ]);
+
+});
+
+$app->get("/admin/categories/create",function(){
+
+    User::verifyLogin();
+    
+    $page = new PageAdmin();
+
+    $page->setTpl("categories-create");
+
+});
+
+$app->post("/admin/categories/create",function(){
+
+    User::verifyLogin();
+    
+    $category = new Category();
+
+    $category->setData($_POST);
+
+    $category->save();
+
+    header('Location:/admin/categories');
+    exit;
+});
+
+$app->get("/admin/categories/:idcategory/delete",function($idcategory){
+
+    User::verifyLogin();
+    
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $category->delete();
+
+    header('Location:/admin/categories');
+    exit;
+});
+
+$app->get("/admin/categories/:idcategor",function($idcategory){
+
+    User::verifyLogin();
+    
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $page = new PageAdmin();
+
+    $page->setTpl("categories-update",[
+        'category'=>$category->getValues()
+    ]);
+   
+});
+
+$app->post("/admin/categories/:idcategor",function($idcategory){
+
+    User::verifyLogin();
+    
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $category->setData($_POST);
+
+    $category ->save();
+
+    header('Location:/admin/categories');
+    exit;   
+});
+
 
 $app->run(); // tudo certo para rodar a página 
 
