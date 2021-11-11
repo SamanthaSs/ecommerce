029550b06a4d725d6d5c754e411c18c397394802
SamanthaSs <samanthaschmoeller@gmail.com>
2021-11-11 16:41:15 -0300

    Frete - não atualiaza o frete e o total

--- site.php
+++ site.php
@@ -67,7 +67,8 @@ $app->get("/cart",function(){
 
     $page->setTpl("cart",[
         'cart'=>$cart->getValues(),
-        'products'=>$cart->getProducts()
+        'products'=>$cart->getProducts(),
+        'error'=>Cart::getMsgError()
     ]);
 
 });
@@ -119,6 +120,32 @@ $app->get("/cart/:idproduct/remove",function($idproduct){
     $cart = Cart::getFromSession();
 
     $cart->removeProduct($product,true);
+
+    header("Location: /cart");
+    exit;
+
+});
+
+$app->get("/cart/:idproduct/remove",function($idproduct){
+
+    $product = new Product();
+
+    $product->get((int)$idproduct);
+
+    $cart = Cart::getFromSession();
+
+    $cart->removeProduct($product,true);
+
+    header("Location: /cart");
+    exit;
+
+});
+
+$app->post("/cart/freight",function() {
+
+    $cart = Cart::getFromSession();
+
+    $cart->setFreight($_POST['zipcode']);
 
     header("Location: /cart");
     exit;
--- views-cache/cart.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/cart.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -9,7 +9,6 @@
         </div>
     </div>
 </div> <!-- End Page title area -->
-
 <div class="single-product-area">
     <div class="zigzag-bottom"></div>
     <div class="container">
@@ -20,10 +19,15 @@
                     <div class="woocommerce">
 
                         <form action="/checkout">
-                            
+
+                            <?php if( $error != '' ){ ?>
+
                             <div class="alert alert-danger" role="alert">
-                            Error!
+                            <?php echo htmlspecialchars( $error, ENT_COMPAT, 'UTF-8', FALSE ); ?>
+
                             </div>
+                            <?php } ?>
+
 
                             <table cellspacing="0" class="shop_table cart">
                                 <thead>
@@ -37,24 +41,21 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <?php $counter1=-1;  if( isset($products) && ( is_array($products) || $products instanceof Traversable ) && sizeof($products) ) foreach( $products as $key1 => $value1 ){ $counter1++; ?>   
+                                    <?php $counter1=-1;  if( isset($products) && ( is_array($products) || $products instanceof Traversable ) && sizeof($products) ) foreach( $products as $key1 => $value1 ){ $counter1++; ?>
+
                                     <tr class="cart_item">
                                         <td class="product-remove">
                                             <a title="Remove this item" class="remove" href="/cart/<?php echo htmlspecialchars( $value1["idproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?>/remove">×</a> 
                                         </td>
-
                                         <td class="product-thumbnail">
                                             <a href="/products/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><img width="145" height="145" alt="poster_1_up" class="shop_thumbnail" src="<?php echo htmlspecialchars( $value1["desphoto"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"></a>
                                         </td>
-
                                         <td class="product-name">
                                             <a href="/product/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["desproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?></a> 
                                         </td>
-
                                         <td class="product-price">
                                             <span class="amount">R$<?php echo formatPrice($value1["vlprice"]); ?></span> 
                                         </td>
-
                                         <td class="product-quantity">
                                             <div class="quantity buttons_added">
                                                 <input type="button" class="minus" value="-" onclick="window.location.href = '/cart/<?php echo htmlspecialchars( $value1["idproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?>/minus'">
@@ -62,61 +63,51 @@
                                                 <input type="button" class="plus" value="+" onclick="window.location.href = '/cart/<?php echo htmlspecialchars( $value1["idproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?>/add'">
                                             </div>
                                         </td>
-
                                         <td class="product-subtotal">
                                             <span class="amount">R$<?php echo formatPrice($value1["vltotal"]); ?></span> 
                                         </td>
                                     </tr>
-                                    <?php } ?>   
+                                    <?php } ?>
+
                                 </tbody>
                             </table>
-
                             <div class="cart-collaterals">
-
                                 <div class="cross-sells">
-
                                     <h2>Cálculo de Frete</h2>
-                                    
+
                                     <div class="coupon">
                                         <label for="cep">CEP:</label>
-                                        <input type="text" placeholder="00000-000" value="" id="cep" class="input-text" name="zipcode">
+                                        <input type="text" placeholder="00000-000" value="<?php echo htmlspecialchars( $cart["deszipcode"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" id="cep" class="input-text" name="zipcode">
                                         <input type="submit" formmethod="post" formaction="/cart/freight" value="CÁLCULAR" class="button">
                                     </div>
 
                                 </div>
-
                                 <div class="cart_totals ">
-
                                     <h2>Resumo da Compra</h2>
-
                                     <table cellspacing="0">
                                         <tbody>
                                             <tr class="cart-subtotal">
                                                 <th>Subtotal</th>
-                                                <td><span class="amount">$700.00</span></td>
+                                                <td><span class="amount">R$<?php echo formatPrice($cart["vlsubtotal"]); ?></span></td>
                                             </tr>
 
                                             <tr class="shipping">
                                                 <th>Frete</th>
-                                                <td>$5.00 <small>prazo de 0 dia(s)</small></td>
+                                                <td>R$<?php echo formatPrice($cart["vlfreight"]); ?><?php if( $cart["nrdays"] > 0 ){ ?> <small>prazo de <?php echo htmlspecialchars( $cart["nrdays"], ENT_COMPAT, 'UTF-8', FALSE ); ?> dia(s)</small><?php } ?></td>
                                             </tr>
 
                                             <tr class="order-total">
                                                 <th>Total</th>
-                                                <td><strong><span class="amount">$705.00</span></strong> </td>
+                                                <td><strong><span class="amount">R$<?php echo formatPrice($cart["vltotal"]); ?></span></strong> </td>
                                             </tr>
                                         </tbody>
                                     </table>
                                 </div>
-
                             </div>
-
                             <div class="pull-right">
                                 <input type="submit" value="Finalizar Compra" name="proceed" class="checkout-button button alt wc-forward">
                             </div>
-
                         </form>
-
                     </div>                        
                 </div>                    
             </div>
